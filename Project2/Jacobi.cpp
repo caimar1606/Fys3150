8105785c8601e74aeb360622104f856f82fec536
lordlindset <henriklindset@gmail.com>
2020-09-25 12:33:35 +0200

    nice commit

--- Project2/Jacobi.cpp
+++ Project2/Jacobi.cpp
@@ -6,11 +6,14 @@
 using namespace arma;
 using namespace std;
 
+ofsteam ofile;
 
-void Jacobi::Initialize(int N, double eps, int S){
+void Jacobi::Initialize(int N, double eps, int S, string filename){
   m_eps = eps;
   m_N = N;
   m_S = S;
+  m_filename = filename;
+  m_R = mat(N-1,N-1,fill::eye)
   mat A = mat(N-1,N-1,fill::zeros);
   for(int i=0;i<N-1;i++){
     for(int j=0;j<N-1;j++){
@@ -34,8 +37,8 @@ int Jacobi::Test(double a_max){
 tuple Jacobi::Findkl(){
   double maxval = 0;
   int k,l;
-  for(int i=0;i<N-1;i++){
-    for(int j=0;j<N-1;j++){
+  for(int i=0;i<m_N-1;i++){
+    for(int j=0;j<m_N-1;j++){
       if(i!=j && abs(m_A(i,j)) > maxval){
         k = i;
         l = j;
@@ -57,13 +60,18 @@ void Jacobi::Computeb(int k, int l){
   s = t*c;
 
   mat B = m_A;
+  mat Rnew = m_R;
 
-  for(int i = 0;i<N-1;i++){
-    for(int j = 0; j<N-1;j++){
+  for(int i = 0;i<m_N-1;i++){
+    for(int j = 0; j<m_N-1;j++){
       if(i!=k && i!=l)
         B(i,k)=m_A(i,k)*c-m_A(i,l)*s;
-        B(i,l)=m_A(i,l)*c-m_A(i,l)*s;
+        B(k,i)=B(i,k);
+        B(i,l)=m_A(i,l)*c+m_A(i,k)*s;
+        B(l,i)=B(i,l)
     }
+    Rnew(i,k)=c*m_R(i,k)-s*m_R(i,l)
+    Rnew(i,l)=c*m_R(i,l)+s*m_R(i,k)
   }
   B(k,k)=m_A(k,k)*c*c-2*m_A(k,l)*c*s+m_A(l,l)*s*s;
   B(l,l)=m_A(l,l)*c*c-2*m_A(k,l)*c*s+m_A(k,k)*s*s;
@@ -71,6 +79,7 @@ void Jacobi::Computeb(int k, int l){
   B(l,k)=0;
 
   m_A = B
+  m_R = Rnew
 }
 
 void Jacobi::solve(){
@@ -94,5 +103,11 @@ void Jacobi::solve(){
 
 
 void Jacobi::print(){
-  
+  ofile.open(m_filename);
+  ofile << setiosflags(ios::showpoint | ios::uppercase);
+  ofile << "eigenvalues:" << endl;
+  for (int i = 0; i < m_N-1; i++){
+    ofile << setw(10) << setprecision(8) << m_A(i,i) << endl;
+  }
+  ofile.close();
 }
--- Project2/project2.hpp
+++ Project2/project2.hpp
@@ -6,14 +6,16 @@ private:
   int m_N;            // Number of intervals in mesh grid
   double m_eps;      //Tolerance
   mat m_A;
+  mat m_R;
   int m_S;
+  string m_filename;
   int Test(double a_max);
   tuple Findkl();
   void ComputeB(int k, int l);
 
 
 public:
-  void Initialize(int N, double eps, int S);
+  void Initialize(int N, double eps, int S, string filename);
   void Solve()
   void Print()
 
